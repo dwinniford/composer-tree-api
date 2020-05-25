cfa61bb2abe1416f2b1bb20b879c569ef835c6b3
dwinniford <dwinniford@gmail.com>
2020-05-25 13:31:11 -0500

    sets origins dynamicallys with credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins Rails.application.credentials.dig(Rails.env.to_sym, :allowed_origins)
     #  needs specific url to not be dangerous
 
     resource '*',
