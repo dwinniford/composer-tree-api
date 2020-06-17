c6364047ab983d61f4ed2e9400aaa23a5401b263
Dave Winniford <dwinniford@gmail.com>
2020-06-17 10:54:05 -0400

    adds attributes csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,16 @@ class ApplicationController < ActionController::API
 
     def cookie 
         "ok"
-        response.headers['Set-Cookie'] = 'SameSite=None;Secure'
+        # response.headers['Set-Cookie'] = 'SameSite=None; Secure'
         pp response.headers 
         # A cookie associated with a resource at http://song-web.herokuapp.com/ was set with `SameSite=None` but without `Secure`.
+        # looks like this: "Set-Cookie"=>"SameSite=None;Secure"
     end
 
     private 
     def set_csrf_cookie
-        cookies["CSRF-TOKEN"] = form_authenticity_token
+        # cookies["CSRF-TOKEN"] = form_authenticity_token
+        response.headers['Set-Cookie'] = "CSRF-TOKEN=#{form_authenticity_token}; SameSite=None; Secure"
        
     end
 end
